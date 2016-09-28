3672f1fa3e501424fc1c184576c0cc8649535304
iPet <0865747@hr.nl>
2016-09-28 21:26:31 +0200

    Nested settings to string

--- lib/seed_dump/dump_methods.rb
+++ lib/seed_dump/dump_methods.rb
@@ -22,7 +22,7 @@ class SeedDump
       # with the composite_primary_keys gem (it returns composite
       # primary key attribute names as hashes).
       if record.class.to_s == "Settings"
-        attribute_strings << (record.value.class == String ? "#{record.var}: '#{record.value}' ": "#{record.var}: #{record.value}")
+        attribute_strings << (record.value.class == String ? "#{record.var}: '#{record.value}' ": "#{record.var}: #{record.value.map { |k, v| [k, "#{v}"] }.to_h}")
       else
         attributes = record.attributes
         attributes.merge!({"remote_data_url" => record.data.url}) if attributes.has_key?("data")
